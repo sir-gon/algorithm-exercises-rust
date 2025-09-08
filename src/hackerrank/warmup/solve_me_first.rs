e66384020494f9ead4da76935d71786e3d25d649
Gonzalo Diaz <devel@gon.cl>
2025-09-07 22:28:29 -0300

    [REFACTOR] Trying to re-organize source code and tests

---
+++ src/hackerrank/warmup/solve_me_first.rs
@@ -0,0 +1,5 @@
+// @link Problem definition [[docs/hackerrank/warmup/solveMeFirst.md]]
+
+pub fn solve_me_first(left: i32, right: i32) -> i32 {
+    left + right
+}
---
+++ tests/hackerrank/warmup/solve_me_first.rs
@@ -0,0 +1,32 @@
+use exercises::hackerrank::warmup::solve_me_first::solve_me_first as add;
+use once_cell::sync::Lazy;
+use serde::Deserialize;
+
+use crate::common;
+use common::utils::load_json;
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[derive(Debug, Deserialize)]
+  struct SolveMeFirstTestCase {
+      title: String,
+      a: i32,
+      b: i32,
+      result: i32
+  }
+
+  static TEST_DATA: Lazy<Vec<SolveMeFirstTestCase>> =
+      Lazy::new(|| load_json("tests/data/hackerrank/warmup/solve_me_first.testcases.json"));
+
+  #[test]
+  fn test_solve_me_first() {
+    for test_case in TEST_DATA.iter() {
+      println!("Testing hackerrank::warmup::solve_me_first::solve_me_first() : {:?}", test_case.title);
+
+      let result = add(test_case.a, test_case.b);
+      assert_eq!(result, test_case.result);
+    }
+  }
+}
