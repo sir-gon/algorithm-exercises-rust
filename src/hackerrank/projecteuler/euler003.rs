2fc4d786d863364b5e2bbfbe613e3b1c623dd8c2
Gonzalo Diaz <devel@gon.cl>
2026-02-18 16:33:39 -0300

    [Hacker Rank]: Project Euler #3: Largest prime factor. Coverage increased.

--- src/hackerrank/projecteuler/euler003.rs
+++ src/hackerrank/projecteuler/euler003.rs
@@ -1,28 +1,27 @@
 // @link Problem definition [[docs/hackerrank/projecteuler/euler003.md]]
 // @link Solution notes [[docs/hackerrank/projecteuler/euler003-solution-notes.md]]
 
-pub fn euler003(n: i64) -> i64 {
+pub fn euler003(n: i64) -> Option<i64> {
     if n < 2 {
-        return 0;
+        return None;
     }
 
     let mut divisor = n;
-    let mut max_prime_factor = None;
+    let mut max_prime_factor: Option<i64> = None;
 
     let mut i = 2;
-    while i * i <= divisor {
+    while i <= (divisor as f64).sqrt() as i64 {
         if 0 == divisor % i {
-            while divisor % i == 0 {
-                divisor /= i;
-            }
-            max_prime_factor = Some(i);
+            divisor /= i;
+            max_prime_factor = Some(divisor);
         }
+
         i += 1;
     }
 
-    if divisor > 1 {
-        return divisor;
+    if max_prime_factor.is_none() {
+        max_prime_factor = Some(n);
     }
 
-    max_prime_factor.unwrap_or(n)
+    Some(max_prime_factor.unwrap_or(n))
 }
